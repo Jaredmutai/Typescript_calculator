2ef769b29935bd67c8588f6746f5807cfb17e28e
Jaredmutai <jaredmutai01j@gmail.com>
2025-03-22 23:28:28 +0300

    added subtract function to the calculator

--- src/ts-version/calculator.ts
+++ src/ts-version/calculator.ts
@@ -2,3 +2,8 @@ function add(a: number, b: number): number {
     return a + b;
 }
 console.log(add(5, 3));
+
+function subtract(a: number, b: number): number{
+    return a - b;
+}
+    console.log(subtract(5, 3));
